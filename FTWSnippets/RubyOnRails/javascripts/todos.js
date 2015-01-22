206ce6b2b17c8db0d3204495336913fb3cc8272a
mhrudow <mhrudow@users.noreply.github.com>
2015-01-21 21:06:01 -0500

    Create todos.js

---
+++ FTWSnippets/RubyOnRails/javascripts/todos.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.notes').hide();
+  $('.best_in_place').best_in_place();
+  $('button').on('click', function(){
+    $(this).next('.notes').slideToggle();
+  }); 
+  $('.pos').focusout(function(){
+    location.reload();
+  });
+  $('.checkbox').click(function(){
+    $('.checkbox').mouseleave(function(){
+      location.reload();
+    });
+  });
+});
